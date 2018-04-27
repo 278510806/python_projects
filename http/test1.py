830694bc285fe2d89a570608cfadc8442be3b44d
shch <shangcheng136@163.com>
2018-04-27 14:48:33 +0800

    http

---
+++ http/test1.py
@@ -0,0 +1,12 @@
+import http.client
+
+conn=http.client.HTTPSConnection('www.baidu.com')
+#发送请求
+conn.request('GET','/')
+#获取响应
+r=conn.getresponse()
+
+print(r.status,r.reason)
+
+for line in r.readlines():
+    print(line)
